46e6c697b90cbd3a17a9e60db707e77268c6c5c1
ChengPeng <2410208224@qq.com>
2024-01-03 15:52:37 +0800

    权限控制

--- src/main/java/com/cp/config/WebConfigurer.java
+++ src/main/java/com/cp/config/WebConfigurer.java
@@ -1,6 +1,8 @@
 package com.cp.config;
 
+import com.cp.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,12 +18,12 @@ public class WebConfigurer implements WebMvcConfigurer {
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
     //@Override
-    //public void addInterceptors(InterceptorRegistry registry) {
-    //    // 把登录的拦截器配置上才能起作用
-    //    //addPathPatterns("/**")拦截所有请求
-    //    //excludePathPatterns("/user/login","/user/toLogin","/static/**")不需要拦截这些请求
-    //    registry.addInterceptor(new LoginInterceptor())
-    //            .addPathPatterns("/**")
-    //            .excludePathPatterns("/user/toLogin", "/user/login", "/auth/code", "/static/**");
-    //}
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 把登录的拦截器配置上才能起作用
+        //addPathPatterns("/**")拦截所有请求
+        //excludePathPatterns("/user/login","/user/toLogin","/static/**")不需要拦截这些请求
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/toLogin", "/user/login", "/auth/code", "/static/**");
+    }
 }
